afcb6d63d472e4645a65420245788718f53f1839
Kristof Eilers <127109500+KristofEilers@users.noreply.github.com>
2023-06-16 14:46:40 +0200

    created components and pages structure

---
+++ components/homepage/CallToActionSection.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CallToActionSection() {
+  return <div>CallToActionSection</div>;
+}
---
+++ pages/MyStudentProfile.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MyStudentProfile() {
+  return <div>MyStudentProfile</div>;
+}
