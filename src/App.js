96b50b18425902c3bd8a1dbd3da607f10cebe9bb
Kristof Eilers <127109500+KristofEilers@users.noreply.github.com>
2023-06-19 17:52:00 +0200

    adding feature section

--- src/App.js
+++ src/App.js
@@ -1,87 +1,12 @@
 import React from "react";
 
 import "./App.css";
+import AppPreviewSection from "./components/homepage/AppPreviewSection";
 
 function App() {
   return (
     <>
-      <h1 className="font-Epiloque font-black text-6xl text-red-400">
-        HeyDaisy
-      </h1>
-
-      {/* DOPDOWN MENU */}
-      <div className=" font-Epiloque navbar bg-base-300 rounded-box">
-        <div className="flex-1 px-2 lg:flex-none">
-          <a className="font-Epiloque text-lg font-bold">daisyUI</a>
-        </div>
-        <div className="flex justify-end flex-1 px-2">
-          <div className="flex items-stretch">
-            <a className="btn btn-ghost rounded-btn">Button</a>
-            <div className="dropdown dropdown-end">
-              <label tabIndex={0} className="btn btn-ghost rounded-btn">
-                Dropdown
-              </label>
-              <ul
-                tabIndex={0}
-                className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4"
-              >
-                <li>
-                  <a>Item 1</a>
-                </li>
-                <li>
-                  <a>Item 2</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* HERO SECTION */}
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-          </div>
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <div className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="email"
-                  className="input input-bordered"
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="password"
-                  className="input input-bordered"
-                />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">
-                    Forgot password?
-                  </a>
-                </label>
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <AppPreviewSection />
     </>
   );
 }
--- src/components/homepage/AppPreviewSection.js
+++ src/components/homepage/AppPreviewSection.js
@@ -1,5 +1,146 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../../App.css";
 
-export default function AppPreviewSection() {
-  return <div>AppPreviewSection</div>;
+function AppPreviewSection() {
+  const [activeTab, setActiveTab] = useState(2); // Initially set Tab 2 as active
+
+  const handleTabClick = (tabIndex) => {
+    setActiveTab(tabIndex);
+  };
+
+  const renderTabText = () => {
+    switch (activeTab) {
+      case 1:
+        return (
+          <div className="text-content">
+            <h3 className="my-3 font-semibold text-2xl">Video</h3>
+            <p>
+              "Get ready to level up your connections with our cutting-edge
+              video call feature! Say goodbye to boring, one-dimensional
+              conversations and embrace the power of face-to-face interaction,
+              no matter where you are. Engage in dynamic discussions, share your
+              screen, and build genuine relationships that go beyond words. It's
+              time to take your conversations to the next level!"
+            </p>
+          </div>
+        );
+      case 2:
+        return (
+          <div className="text-content">
+            <h3 className="my-3 font-semibold text-2xl">Chat</h3>
+            <p>
+              "Unleash the power of words with our chat feature that's designed
+              to ignite instant connections and foster meaningful conversations.
+              Whether you're seeking language exchange partners, language
+              enthusiasts, or cultural enthusiasts, our chat feature brings you
+              a vibrant community at your fingertips. Share your language
+              journey, exchange tips, and make lifelong friendships on the go.
+              Get ready to dive into a world of limitless linguistic
+              possibilities!"
+            </p>
+          </div>
+        );
+      case 3:
+        return (
+          <div className="text-content">
+            <h3 className="my-3 font-semibold text-2xl">Events</h3>
+            <p>
+              "Experience the thrill of cultural immersion with our exciting
+              events feature! From virtual language workshops and global
+              language festivals to interactive language challenges, our events
+              are a gateway to a world of diverse cultures and languages.
+              Connect with like-minded language enthusiasts, expand your
+              horizons, and embrace the joy of learning together. With our
+              events, every day is a celebration of languages, cultures, and
+              unforgettable experiences!"
+            </p>
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
+
+  const renderTabImage = () => {
+    switch (activeTab) {
+      case 1:
+        return (
+          <div className="image-content">
+            <p>Video</p>
+            <img
+              src="https://via.placeholder.com/300x500"
+              alt="Video Placeholder"
+            />
+          </div>
+        );
+      case 2:
+        return (
+          <div className="image-content">
+            <p>Chat</p>
+            <img
+              src="https://via.placeholder.com/300x500"
+              alt="Chat Placeholder"
+            />
+          </div>
+        );
+      case 3:
+        return (
+          <div className="image-content">
+            <p>Events</p>
+            <img
+              src="https://via.placeholder.com/300x500"
+              alt="Events Placeholder"
+            />
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <>
+      <div className="flex md:container md:mx-auto justify-center content-center p-10">
+        <div className="flex content-center gap-10">
+          <div className="max-w-md p-5">
+            <h2 className=" font-bold text-4xl mb-5">Features</h2>
+            <div className="tabs tabs-boxed mb-5">
+              <Link
+                className={`tab ${activeTab === 1 ? "tab-active" : ""}`}
+                onClick={() => handleTabClick(1)}
+              >
+                Video
+              </Link>
+              <Link
+                className={`tab ${activeTab === 2 ? "tab-active" : ""}`}
+                onClick={() => handleTabClick(2)}
+              >
+                Chat
+              </Link>
+              <Link
+                className={`tab ${activeTab === 3 ? "tab-active" : ""}`}
+                onClick={() => handleTabClick(3)}
+              >
+                Events
+              </Link>
+            </div>
+            <div>{renderTabText()}</div>
+          </div>
+          <div className="">
+            <div className="mockup-phone border-primary">
+              <div className="camera"></div>
+              <div className="display">
+                <div className="artboard artboard-demo phone-1">
+                  {renderTabImage()}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
+
+export default AppPreviewSection;
