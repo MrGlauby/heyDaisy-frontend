55a144fe37be36742f50566d4e0f48a597fa809f
MariaA-109 <122259443+MariaA-109@users.noreply.github.com>
2023-06-19 13:50:26 +0200

    added event section, calltoaction section & footer

--- src/components/homepage/CallToActionSection.js
+++ src/components/homepage/CallToActionSection.js
@@ -1,5 +1,25 @@
 import React from "react";
 
 export default function CallToActionSection() {
-  return <div>CallToActionSection</div>;
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Interested in joining us?</h1>
+            <p className="py-6">
+              Join our language learning revolution! With our innovative app,
+              you can effortlessly master languages through tailored lessons,
+              interactive exercises, and expertly designed curriculum. Whether
+              you're a beginner or advanced learner, we have everything you need
+              to propel your fluency. Don't miss this incredible opportunity to
+              unlock a world of linguistic possibilities. Register now and
+              embark on your language learning adventure today!
+            </p>
+            <button className="btn btn-primary">REGISTER</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/homepage/EventsSection.js
+++ src/components/homepage/EventsSection.js
@@ -1,5 +1,142 @@
 import React from "react";
 
 export default function EventsSection() {
-  return <div>EventsSection</div>;
+  return (
+    <div>
+      <h1 className="text-5xl font-bold ">Events</h1>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Language</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td>
+                <div className="flex items-center space-x-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src="/tailwind-css-component-profile-2@56w.png"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <div className="font-bold">Hart Hagerty</div>
+                    <div className="text-sm opacity-50">United States</div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                Let's talk about soccer!
+                <br />
+                <span className="badge badge-ghost badge-sm">
+                  advanced level
+                </span>
+              </td>
+              <td>Spanish</td>
+              <th>
+                <button className="btn btn-ghost btn-xs">details</button>
+              </th>
+            </tr>
+            {/* row 2 */}
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td>
+                <div className="flex items-center space-x-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src="/tailwind-css-component-profile-3@56w.png"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <div className="font-bold">Brice Swyre</div>
+                    <div className="text-sm opacity-50">China</div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                I want to talk about traveling :)
+                <br />
+                <span className="badge badge-ghost badge-sm">basic level</span>
+              </td>
+              <td>Italian</td>
+              <th>
+                <button className="btn btn-ghost btn-xs">details</button>
+              </th>
+            </tr>
+            {/* row 3 */}
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td>
+                <div className="flex items-center space-x-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src="/tailwind-css-component-profile-4@56w.png"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <div className="font-bold">Marjy Ferencz</div>
+                    <div className="text-sm opacity-50">Russia</div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                Let's cook a Portuguese dish together!
+                <br />
+                <span className="badge badge-ghost badge-sm">
+                  advanced level
+                </span>
+              </td>
+              <td>Portuguese</td>
+              <th>
+                <button className="btn btn-ghost btn-xs">details</button>
+              </th>
+            </tr>
+            {/* row 4 */}
+          </tbody>
+          {/* foot */}
+          <tfoot>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Language</th>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    </div>
+  );
 }
