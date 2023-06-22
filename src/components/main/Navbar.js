020bfab7ed075a7fe88297f59689749526bc791a
MariaA-109 <122259443+MariaA-109@users.noreply.github.com>
2023-06-22 10:05:24 +0200

    changed links in the navbar and added some margin

--- src/components/main/Navbar.js
+++ src/components/main/Navbar.js
@@ -1,41 +1,37 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import logo from "../../img/heyDaisy1.png";
 
 export default function Navbar() {
   return (
     <div>
-      <div className="navbar   bg-indigo-200 p-5 ">
+      <div className="navbar   bg-violet-50 p-5 ">
         <div className="flex-1 ">
-          <a href="home">
+          <Link to={"/"}>
             <img
               className="logo flex self-start h-24 w-24 transition duration-150 ease-in-out hover:scale-110"
               src={logo}
               alt="logo"
             />
-          </a>
+          </Link>
         </div>
 
-        <div className="flex-none  ">
-          <ul className="menu menu-horizontal px-1 text-lg 	 ">
+        <div>
+          <ul className="menu menu-horizontal  text-lg">
             <li className="p-5">
-              <a
-                href="Register"
-                target="_blank"
-                className="shadow-xl bg-indigo-100 "
-              >
-                Register
-              </a>
+              <Link to={"/register"}>
+                <button className="btn btn-primary">Register</button>
+              </Link>
             </li>
             <li className="p-5">
-              <a href="Login" className="shadow-xl bg-indigo-100 ">
-                Login
-              </a>
+              <Link to={"/login"}>
+                <button className="btn btn-primary">Login</button>
+              </Link>
             </li>
             <li className="p-5">
-              <a href="About" className="shadow-xl bg-indigo-100 ">
-                About Us
-              </a>
+              <Link to={"/about"}>
+                <button className="btn btn-primary">About us</button>
+              </Link>
             </li>
           </ul>
         </div>
