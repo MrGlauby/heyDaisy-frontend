0f91a3385d0bed39fa283a759097f0d379785ab9
Kristof Eilers <127109500+KristofEilers@users.noreply.github.com>
2023-06-23 13:47:32 +0200

    fixing links in the navbar and removed video component

--- src/components/main/Navbar.js
+++ src/components/main/Navbar.js
@@ -1,30 +1,47 @@
 import React from "react";
 
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 import logo from "../../img/heyDaisy1.png";
 
 export default function Navbar() {
-  return <div>
-            <div className="navbar   bg-indigo-200 p-5 " >
-                
-                <div className="flex-1 ">
-                  <Link to="home">
-                  <img className="logoflex self-start  pl-2  h-35 w-full m-left-4px  min-w-[6rem]   max-w-[12rem]   duration-150 ease-in-out hover:scale-110" src={logo} alt="logo" /></Link>
-                </div>
-                
-                <div className="flex-none  ">
-                    <ul className="menu menu-horizontal">
-                      <li className="p-5   text-lg " ><Link to="register" target="_blank" className="shadow-xl bg-indigo-100">Register</Link></li>
-                      <li className="p-5 text-lg"><Link to="Login" className="shadow-xl bg-indigo-100 ">Login</Link></li>
-                      <li className="p-5 text-lg"><Link to="About" className= "shadow-xl bg-indigo-100 ">About Us</Link></li>
-                    </ul>
-                </div>
-            </div>
-
+  return (
+    <div>
+      <div className="navbar   bg-indigo-200 p-5 ">
+        <div className="flex-1 ">
+          <Link to="/">
+            <img
+              className="logoflex self-start  pl-2  h-35 w-full m-left-4px  min-w-[6rem]   max-w-[12rem]   duration-150 ease-in-out hover:scale-110"
+              src={logo}
+              alt="logo"
+            />
+          </Link>
         </div>
+
+        <div className="flex-none  ">
+          <ul className="menu menu-horizontal">
+            <li className="p-5 text-lg ">
+              <Link
+                to="/register"
+                target="_blank"
+                className="shadow-xl bg-indigo-100"
+              >
+                Register
+              </Link>
+            </li>
+            <li className="p-5 text-lg">
+              <Link to="/Login" className="shadow-xl bg-indigo-100 ">
+                Login
+              </Link>
+            </li>
+            <li className="p-5 text-lg">
+              <Link to="/About" className="shadow-xl bg-indigo-100 ">
+                About Us
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-
-
