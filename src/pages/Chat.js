32b7c174893165f3b1e7d30d483e374a6be8f8fa
Christoph Nientied <christophnientied@googlemail.com>
2023-06-27 13:22:40 +0200

    accidentally worked on main, but made only minor changes for socket.io

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,10 +1,25 @@
 import "../../src/App.css";
-// import io from "socket.io-client";
 import { useState } from "react";
 import AuthPage from "../components/chat/AuthPage";
 import ChatPage from "../components/chat/ChatPage";
 import Navbar from "../components/main/Navbar";
 import Footer from "../components/main/Footer";
+
+import io from 'socket.io-client';
+
+// Connect to the server
+const socket = io('http://localhost:3002');
+
+// Send a message
+socket.emit('message', 'Hello, server!');
+
+// Receive a message
+socket.on('message', message => {
+  console.log('Received message:', message);
+});
+
+
+
 
 function Chat() {
     const [user, setUser] = useState(undefined);
