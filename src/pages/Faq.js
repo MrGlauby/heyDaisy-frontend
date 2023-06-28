33aae8b78c42ac0eb3041159805f2581b0302702
Kristof Eilers <127109500+KristofEilers@users.noreply.github.com>
2023-06-28 15:03:05 +0200

    adding private Route

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -7,7 +7,35 @@ export default function Faq() {
   return (
     <div>
       <Navbar />
-      <div>Was ist hier los?</div>
+      <div className="min-h-screen">
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="radio" name="my-accordion-2" checked="checked" />
+          <div className="collapse-title text-xl font-medium">
+            Click to open this one and close others
+          </div>
+          <div className="collapse-content">
+            <p>hello</p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            Click to open this one and close others
+          </div>
+          <div className="collapse-content">
+            <p>hello</p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            Click to open this one and close others
+          </div>
+          <div className="collapse-content">
+            <p>hello</p>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
