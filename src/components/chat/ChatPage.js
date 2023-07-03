21ebcc19a6f8d7cc23b63ac38dc652ab84ad97e4
MariaA-109 <122259443+MariaA-109@users.noreply.github.com>
2023-07-03 11:21:36 +0200

    added loading spinner and updated the styling of some components

--- src/components/chat/ChatPage.js
+++ src/components/chat/ChatPage.js
@@ -1,18 +1,22 @@
-import {MultiChatSocket, MultiChatWindow, useMultiChatLogic } from "react-chat-engine-advanced";
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
 
 const ChatPage = (props) => {
-    const chatProps = useMultiChatLogic(
-        "286c08c5-5015-484c-94f9-c980b4e6c57e", 
-        props.user.username, 
-        props.user.secret
-        );
-    
-    return (
-    <div style={{height: "100vh"}}>
-        <MultiChatSocket {...chatProps} />
-        <MultiChatWindow {...chatProps} style={{height:"80%"}} />
+  const chatProps = useMultiChatLogic(
+    "286c08c5-5015-484c-94f9-c980b4e6c57e",
+    props.user.username,
+    props.user.secret
+  );
+
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "80%" }} />
     </div>
-    )
-}
+  );
+};
 
 export default ChatPage;
